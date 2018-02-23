9caf4514a68477dc4c552a0a957dc746ed564540
Aaron Langley <aaronlangley@wsip-184-188-122-178.oc.oc.cox.net>
2018-02-23 15:02:03 -0800

    footer functionality

--- src/app/components/partials/navbar/navbar.component.ts
+++ src/app/components/partials/navbar/navbar.component.ts
@@ -1,28 +1,28 @@
 import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
-import { Util } from './../../../helpers/util.helper';
-import { User } from './../../../models/user.model';
+import { Util } from '../../../helpers/util.helper';
+import { User } from '../../../models/user.model';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush,//this means it is not active checking for data changes
+  changeDetection: ChangeDetectionStrategy.OnPush, // this means it is not active checking for data changes
 })
 export class NavbarComponent implements OnInit {
-  user:User;
+  user: User;
   env;
-  constructor(private cd:ChangeDetectorRef) {
+  constructor(private cd: ChangeDetectorRef) {
   }
 
   ngOnInit() {
     this.user = User.Auth();
     this.env = Util.env;
-    //I am intentionally over complicating this here to show how useful this can become in a large app
-    User.on(['auth', 'saveApi'], (auth_state)=>{//data will be different depending on which event was emitted
+    // I am intentionally over complicating this here to show how useful this can become in a large app
+    User.on(['auth', 'saveApi'], (auth_state) => {// data will be different depending on which event was emitted
       console.log('the user has:', auth_state);
       this.user = User.Auth();
-      //we can dynamically make the view check on cvertain events. For large apps this is very efficient
-      this.cd.markForCheck();//this makes the view check for updates once
+      // we can dynamically make the view check on cvertain events. For large apps this is very efficient
+      this.cd.markForCheck(); // this makes the view check for updates once
     });
 
   }
